ae94e450cd75613e32a34b2d9b0f4c2ea1dc94e2
bansodeshubh <bansodeshubham262@gmail.com>
2024-09-24 18:09:07 +0530

    6th commite

--- com.MavenBasic/src/test/java/com/maven/CrossBrowsing.java
+++ com.MavenBasic/src/test/java/com/maven/CrossBrowsing.java
@@ -1,30 +1,30 @@
-package com.maven;
+// package com.maven;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.firefox.FirefoxDriver;
+// import org.testng.annotations.BeforeClass;
+// import org.testng.annotations.Parameters;
 
-import com.microsoft.edge.seleniumtools.EdgeDriver;
+// import com.microsoft.edge.seleniumtools.EdgeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+// import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class CrossBrowsing {
-	 static WebDriver driver;
+// public class CrossBrowsing {
+// 	 static WebDriver driver;
 
-	@Parameters("browser")
-	@BeforeClass
-	public static void openBrowser(String browser) {
-		if (browser.equalsIgnoreCase("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
-		} else if (browser.equalsIgnoreCase("edge")) {
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
-		} else if (browser.equalsIgnoreCase("firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-		}
-	}
-}
+// 	@Parameters("browser")
+// 	@BeforeClass
+// 	public static void openBrowser(String browser) {
+// 		if (browser.equalsIgnoreCase("chrome")) {
+// 			WebDriverManager.chromedriver().setup();
+// 			driver = new ChromeDriver();
+// 		} else if (browser.equalsIgnoreCase("edge")) {
+// 			WebDriverManager.edgedriver().setup();
+// 			driver = new EdgeDriver();
+// 		} else if (browser.equalsIgnoreCase("firefox")) {
+// 			WebDriverManager.firefoxdriver().setup();
+// 			driver = new FirefoxDriver();
+// 		}
+// 	}
+// }
--- com.MavenBasic/src/test/java/com/maven/ReportTest.java
+++ com.MavenBasic/src/test/java/com/maven/ReportTest.java
@@ -1,117 +1,117 @@
-package com.maven;
+// package com.maven;
 
 
-import java.io.IOException;
-import java.lang.reflect.Method;
+// import java.io.IOException;
+// import java.lang.reflect.Method;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.WebDriver;
+// import org.testng.Assert;
+// import org.testng.ITestResult;
+// import org.testng.annotations.AfterClass;
+// import org.testng.annotations.AfterMethod;
+// import org.testng.annotations.AfterTest;
+// import org.testng.annotations.BeforeClass;
+// import org.testng.annotations.BeforeMethod;
+// import org.testng.annotations.BeforeTest;
+// import org.testng.annotations.Test;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
+// import com.aventstack.extentreports.ExtentReports;
+// import com.aventstack.extentreports.ExtentTest;
+// import com.aventstack.extentreports.Status;
+// import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+// import com.aventstack.extentreports.reporter.configuration.Theme;
 
-public class ReportTest {
+// public class ReportTest {
 
-	public static WebDriver driver;
-	public static ExtentHtmlReporter htmlreporter;
-	public static ExtentReports extent;
-	public static ExtentTest test;
+// 	public static WebDriver driver;
+// 	public static ExtentHtmlReporter htmlreporter;
+// 	public static ExtentReports extent;
+// 	public static ExtentTest test;
 
-	@BeforeTest
-	static public void setupExtentEnv() {
+// 	@BeforeTest
+// 	static public void setupExtentEnv() {
 		
-		htmlreporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/extent-report.html");
-		htmlreporter.config().setDocumentTitle("Facebook Log In Report");
-		htmlreporter.config().setReportName("Facebook  Log In Testing");
-		htmlreporter.config().setTheme(Theme.STANDARD);
+// 		htmlreporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/extent-report.html");
+// 		htmlreporter.config().setDocumentTitle("Facebook Log In Report");
+// 		htmlreporter.config().setReportName("Facebook  Log In Testing");
+// 		htmlreporter.config().setTheme(Theme.STANDARD);
 		
-		extent = new ExtentReports();
-		extent.attachReporter(htmlreporter);
-		extent.setSystemInfo("HOST NAME", "LOCALHOST");
-		extent.setSystemInfo("Operating System", "WINDOWS 10");   //before test ===> report 
-		extent.setSystemInfo("Tester NAME", "Shubham Bansode");   //before class==> browser
-		extent.setSystemInfo("Browser", "Chrome");               //before method ==> test
-	}
+// 		extent = new ExtentReports();
+// 		extent.attachReporter(htmlreporter);
+// 		extent.setSystemInfo("HOST NAME", "LOCALHOST");
+// 		extent.setSystemInfo("Operating System", "WINDOWS 10");   //before test ===> report 
+// 		extent.setSystemInfo("Tester NAME", "Shubham Bansode");   //before class==> browser
+// 		extent.setSystemInfo("Browser", "Chrome");               //before method ==> test
+// 	}
 
 	
-	@BeforeClass
-	public void openBrowser() {
+// 	@BeforeClass
+// 	public void openBrowser() {
 		
-		//driver = new ChromeDriver();
-	    CrossBrowsing.openBrowser("chrome");
-		driver.get("http://www.facebook.com");
-		driver.manage().window().maximize();
-	}
+// 		//driver = new ChromeDriver();
+// 	    CrossBrowsing.openBrowser("chrome");
+// 		driver.get("http://www.facebook.com");
+// 		driver.manage().window().maximize();
+// 	}
 	
-	@BeforeMethod
-	public void register(Method fb ) {   
+// 	@BeforeMethod
+// 	public void register(Method fb ) {   
 		
-		test = extent.createTest(fb.getName());
-	}
+// 		test = extent.createTest(fb.getName());
+// 	}
 
-	@Test(priority = 2)
-	public void verifyHomePageTitle() {
-		String expectedTitle = "Facebook � log in or sign up";
-		String actualTitle = driver.getTitle();
-		Assert.assertEquals(actualTitle, expectedTitle);
-	} 
+// 	@Test(priority = 2)
+// 	public void verifyHomePageTitle() {
+// 		String expectedTitle = "Facebook � log in or sign up";
+// 		String actualTitle = driver.getTitle();
+// 		Assert.assertEquals(actualTitle, expectedTitle);
+// 	} 
 
-	@Test(priority = 1)
-	public void verifyFacebookImgDisplayed() {
-		boolean arrivalsActual = driver.findElement(By.xpath("//img[@alt='Facebook']"))
-				.isDisplayed();
-		Assert.assertTrue(arrivalsActual);
-	}
+// 	@Test(priority = 1)
+// 	public void verifyFacebookImgDisplayed() {
+// 		boolean arrivalsActual = driver.findElement(By.xpath("//img[@alt='Facebook']"))
+// 				.isDisplayed();
+// 		Assert.assertTrue(arrivalsActual);
+// 	}
 
-	@Test
-	public void verifyLogIn() {
-		String expectedlogin = "Log In";
-		String actuallogin = driver.findElement(By.xpath("//*[text()='Log In']")).getText();
-		Assert.assertEquals(actuallogin, expectedlogin);
-	}
+// 	@Test
+// 	public void verifyLogIn() {
+// 		String expectedlogin = "Log In";
+// 		String actuallogin = driver.findElement(By.xpath("//*[text()='Log In']")).getText();
+// 		Assert.assertEquals(actuallogin, expectedlogin);
+// 	}
 
-	@AfterMethod
-	public void tearDown(ITestResult result) throws IOException {
+// 	@AfterMethod
+// 	public void tearDown(ITestResult result) throws IOException {
 
-            switch (result.getStatus()) {
-                case ITestResult.FAILURE:
-                    test.log(Status.FAIL, "TEST CASE FAILED is" + result.getName());
-                    test.log(Status.FAIL, "TEST CASE FAILED is" + result.getThrowable());
-                    String screenshotpath = ScreenShot.getScreenshot(driver, result.getName());
-                    test.addScreenCaptureFromPath(screenshotpath);
-                    break;
-                case ITestResult.SKIP:
-                    test.log(Status.SKIP, "TEST CASE SkIPPED:" + result.getName());
-                    test.log(Status.SKIP, "TEST CASE FAILED is" + result.getThrowable());
-                    break;
-                case ITestResult.SUCCESS:
-                    test.log(Status.PASS, "TEST CASE PASSED:" + result.getName());
-                    break;
-                default:
-                    break;
-            }
+//             switch (result.getStatus()) {
+//                 case ITestResult.FAILURE:
+//                     test.log(Status.FAIL, "TEST CASE FAILED is" + result.getName());
+//                     test.log(Status.FAIL, "TEST CASE FAILED is" + result.getThrowable());
+//                     String screenshotpath = ScreenShot.getScreenshot(driver, result.getName());
+//                     test.addScreenCaptureFromPath(screenshotpath);
+//                     break;
+//                 case ITestResult.SKIP:
+//                     test.log(Status.SKIP, "TEST CASE SkIPPED:" + result.getName());
+//                     test.log(Status.SKIP, "TEST CASE FAILED is" + result.getThrowable());
+//                     break;
+//                 case ITestResult.SUCCESS:
+//                     test.log(Status.PASS, "TEST CASE PASSED:" + result.getName());
+//                     break;
+//                 default:
+//                     break;
+//             }
 	
-	}
+// 	}
 	
-	@AfterTest
-	public void cleanup() {
-		extent.flush();
+// 	@AfterTest
+// 	public void cleanup() {
+// 		extent.flush();
 
-	}
-	@AfterClass
-	public void browserTeardown() {
-		driver.quit();
-	}
-}
+// 	}
+// 	@AfterClass
+// 	public void browserTeardown() {
+// 		driver.quit();
+// 	}
+// }
