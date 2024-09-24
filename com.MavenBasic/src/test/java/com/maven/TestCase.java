952cdbad1668a6cfc0ecc85b3dd8d4ddb33433a5
bansodeshubh <bansodeshubham262@gmail.com>
2024-09-24 19:46:09 +0530

    edgeDriver

--- com.MavenBasic/src/test/java/com/maven/TestCase.java
+++ com.MavenBasic/src/test/java/com/maven/TestCase.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -16,8 +16,8 @@ public class TestCase {
 	@Test
 	public void test1() {
 //		1) Open the browser
-        WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+        WebDriverManager.edgedriver().setup();
+		driver = new EdgeDriver();
  		driver.manage().window().maximize();
 	}
 
